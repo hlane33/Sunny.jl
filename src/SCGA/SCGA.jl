1a09670477f2d00e60aedf03d31e7bdce2acb70d
hlane33 <119619760+hlane33@users.noreply.github.com>
2024-09-12 15:39:23 +0100

    Start SCGA branch
    update debug
    
    add example
    
    Fixed phases in J(q)
    
    Clean up
    
    working up to scale factor
    
    Add anisotropy and second loss function
    
    Clean up code
    
    Preallocate arrays
    
    Tidy up functions
    
    Start implementation of SU(N) SCGA
    
    Add SUN SCGA
    
    Begin adding SCGA Type
    
    Implement new intensities function
    
    Fix form factors
    
    Change default sum rule to quantum
    
    Add tests.
    
    Fix test
    
    Fix texts and lower bound for optimizer
    
    Pass kwargs to Optim.Options
    
    Fix anisotropy factor of 2 and add test
    
    Tidy up functions
    
    Start implementation of SU(N) SCGA
    
    Add SUN SCGA
    
    Begin adding SCGA Type
    
    Implement new intensities function
    
    Fix form factors
    
    Fix anisotropy factor of 2 and add test
    
    Add option for Newton's Method.

---
+++ src/SCGA/SCGA.jl
@@ -0,0 +1,249 @@
+abstract type AbstractSCGA end
+
+struct SCGA <: AbstractSCGA
+    sys            :: System
+    measure        :: MeasureSpec
+    regularization :: Float64
+end
+
+"""
+    SCGA(sys::System; measure, regularization=1e-8)
+
+Constructs an object to perform the self consistent gaussian approximation. Enables 
+the use of `intensities_static`](@ref) with an SCGA type to calculate the static 
+structure factor in the paramagnetic phase. If the temperature is below the ordering
+temperature, the intensities will be negative.
+"""
+
+function SCGA(sys::System; measure::Union{Nothing, MeasureSpec}, regularization=1e-8)
+    measure = @something measure empty_measurespec(sys)
+    if length(eachsite(sys)) != prod(size(measure.observables)[2:5])
+        error("Size mismatch. Check that measure is built using consistent system.")
+    end
+    return SCGA(sys,measure, regularization)
+end
+
+
+function fourier_transform_interaction_matrix(sys::System; k, ϵ=0)
+    @assert sys.mode in (:dipole, :dipole_large_S) "SU(N) mode not supported"
+    @assert sys.dims == (1, 1, 1) "System must have only a single cell"
+    Na = natoms(sys.crystal)
+    J_k = zeros(ComplexF64, 3, Na, 3, Na)
+
+    for i in 1:Na
+        for coupling in sys.interactions_union[i].pair
+            (; isculled, bond, bilin) = coupling
+            isculled && break
+
+            (; j, n) = bond
+            J = exp(2π * im * dot(k, n+sys.crystal.positions[j]-sys.crystal.positions[i])) * Mat3(bilin*I)
+            J_k[:, i, :, j] += J / 2
+            J_k[:, j, :, i] += J' / 2
+        end
+    end
+
+    if !isnothing(sys.ewald)
+        A = precompute_dipole_ewald_at_wavevector(sys.crystal, (1,1,1), k) * sys.ewald.μ0_μB²
+        A = reshape(A, Na, Na)
+        for i in 1:Na, j in 1:Na
+            J_k[:, i, :, j] += gs[i]' * A[i, j] * gs[j] / 2
+        end
+    end
+    J_k = 2J_k # I'm not so sure why we need this
+    for i in 1:Na
+        onsite_coupling = sys.interactions_union[i].onsite
+        (; c2, c4, c6) = onsite_coupling
+        anisotropy = [c2[1]-c2[3]   c2[5]   0.5c2[2];
+                        c2[5]   -c2[1]-c2[3]  0.5c2[4];
+                        0.5c2[2]    0.5c2[4]    2c2[3]]
+        J_k[:, i, :, i] += 2*anisotropy
+    end
+
+    J_k = reshape(J_k, 3*Na, 3*Na)
+    @assert diffnorm2(J_k, J_k') < 1e-15
+    J_k = hermitianpart(J_k)
+    return J_k 
+end
+
+
+function find_lagrange_multiplier(sys::System,kT; ϵ=0, SumRule = "Classical",starting_offset = 0.2, maxiters=1_000,tol = 1e-10,method = "Nelder Mead")
+    Nq = 8
+    dq = 1/Nq;
+    Na = natoms(sys.crystal)
+    qarray = -0.5: dq : 0.5-dq
+    q = [[qx, qy, qz] for qx in qarray, qy in qarray, qz in qarray]
+    Jq_array = [fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) for q_in ∈ q] 
+    if SumRule == "Classical"
+        S_sq = sys.κs[1]^2
+    elseif SumRule == "Quantum"
+        S_sq = sys.κs[1] * (sys.κs[1] + 1)
+    else
+        error("Unsupported SumRule: $SumRule. Expected 'Classical' or 'Quantum'.")
+    end
+    println("Sum rule: $SumRule")
+    eig_vals = zeros(3Na,length(Jq_array))
+    for j in 1:length(Jq_array)
+         eig_vals[:,j] .= eigvals(Jq_array[j])
+    end
+    if method == "Nelder Mead"
+        function loss(λ)
+            sum_term = sum(1 ./ (λ .+ (1/(kT)).*eig_vals ))
+            return ((1/(Na*length(q))) * sum_term - S_sq)^2 
+        end
+        lower = -minimum(eig_vals)/kT
+        upper = Inf
+        p = [lower+starting_offset] # some offset from the lower bound.
+        options = Optim.Options(; iterations=maxiters, show_trace=false,g_tol=tol)
+        result = optimize(loss, lower, upper, p,NelderMead(), options)
+        min = Optim.minimizer(result)[1]
+    elseif method == "Newton's Method" #Newton's method 
+        function f(λ)
+            sum_term = sum(1 ./ (λ .+ (1/(kT)).*eig_vals ))
+            return (1/(Na*length(q))) * sum_term  
+        end
+        function J(λ)
+            sum_term = sum((1 ./ (λ .+ (1/(kT)).*eig_vals )).^2)
+            return -(1/(Na*length(q))) * sum_term  
+        end
+        lower = -minimum(eig_vals)/kT
+        λn = starting_offset*0.1+ lower
+        for n ∈ 1:maxiters
+          λ = λn + (1/J(λn))*(S_sq-f(λn))
+          if abs(λ-λn) < tol 
+              println("Newton's method converged to within tolerance, $tol, after $n steps.")
+              min=λ
+              break
+          else
+          λn = λ
+          end
+        end
+        
+    else
+        throw("Please provide valid method for the λ optimization.")
+    end
+    println("Lagrange multiplier: $min")
+    return min
+end
+
+function intensities_static(scga::SCGA, qpts; formfactors=nothing, kT=0.0, SumRule = "Quantum",starting_offset = 0.2, maxiters=1_000,method="Nelder Mead",tol=1e-7)
+    kT == 0.0 && error("kT must be non-zero")
+    qpts = convert(AbstractQPoints, qpts)
+    (; sys, measure, regularization) = scga
+    Na = natoms(sys.crystal)
+    Nq = length(qpts.qs)
+    Nobs = num_observables(measure)
+    λ = find_lagrange_multiplier(sys,kT;SumRule,method,tol,maxiters,starting_offset)
+    intensity = zeros(eltype(measure),Nq)
+    Ncorr = length(measure.corr_pairs)
+    for (iq, q) in enumerate(qpts.qs)
+        pref = zeros(ComplexF64, Nobs, Na)
+        corrbuf = zeros(ComplexF64, Ncorr)
+        intensitybuf = zeros(eltype(measure),3,3)
+        q_reshaped = to_reshaped_rlu(sys, q)
+        q_global = sys.crystal.recipvecs * q
+        for i in 1:Na
+            for μ in 1:3
+                ff = get_swt_formfactor(measure, μ, i)
+                pref[μ, i] = compute_form_factor(ff, norm2(q_global))
+            end
+        end
+        J_mat = fourier_transform_interaction_matrix(sys; k=q_reshaped, ϵ=regularization)
+        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/kT)*J_mat)) # this is [(Iλ+J(q))^-1]^αβ_μν
+        for i ∈ 1:Na 
+            for j ∈ 1:Na 
+                intensitybuf += pref[1,i]*conj(pref[1,j])*inverted_matrix[1+3(i-1):3+3(i-1),1+3(j-1):3+3(j-1)] 
+                # TODO allow different form factor for each observable 
+            end
+        end
+        map!(corrbuf, measure.corr_pairs) do (α, β)
+            intensitybuf[α,β]
+        end
+        intensity[iq] = measure.combiner(q_global, corrbuf)
+    end
+    if extrema(intensity)[1] < -1e-2
+        @warn "Warning: negative intensities! kT is probably below the ordering temperature."
+        # TODO Throw an error. This is for diagnostic purposes.
+    end
+    return StaticIntensities(sys.crystal, qpts, reshape(intensity,size(qpts.qs)))
+end
+
+
+function fourier_transform_deprecated(sys::System; k, ϵ=0)
+    @assert sys.mode in (:dipole, :dipole_large_S) "SU(N) mode not supported"
+    @assert sys.latsize == (1, 1, 1) "System must have only a single cell"
+
+    Na = natoms(sys.crystal)
+    J_k = zeros(ComplexF64, 3, Na, 3, Na)
+
+    for i in 1:Na
+        for coupling in sys.interactions_union[i].pair
+            (; isculled, bond, bilin) = coupling
+            isculled && break
+            (; j, n) = bond
+            J = exp(2π * im * dot(k, n+sys.crystal.positions[j]-sys.crystal.positions[i])) * Mat3(bilin*I)
+            J_k[:, i, :, j] += J / 2
+            J_k[:, j, :, i] += J' / 2
+        end
+    end
+
+    if !isnothing(sys.ewald)
+        A = precompute_dipole_ewald_at_wavevector(sys.crystal, (1,1,1), k) * sys.ewald.μ0_μB²
+        A = reshape(A, Na, Na)
+        for i in 1:Na, j in 1:Na
+            J_k[:, i, :, j] += gs[i]' * A[i, j] * gs[j] / 2
+        end
+    end
+
+    J_k = reshape(J_k, 3*Na, 3*Na)
+    @assert diffnorm2(J_k, J_k') < 1e-15
+    J_k = hermitianpart(J_k)
+    return 2J_k
+end
+
+
+function StructureFactorSCGA_deprecated(sys,T,qs;SumRule = "Classical")
+    Na = natoms(sys.crystal)
+    kb=0.0861733326
+    λ = find_lagrange_multiplier(sys,T;SumRule)
+    Sout = zeros(ComplexF64, 3,3, length(qs))
+    for (qi,q) ∈ enumerate(qs)
+        J_mat = fourier_transform_interaction_matrix(sys; k=q, ϵ=0)
+        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/(kb*T))*J_mat))
+        output_Sab = zeros(ComplexF64, 3,3)
+        index_list = 1:3:3Na 
+        for i ∈ index_list
+            for j ∈ index_list
+                output_Sab += inverted_matrix[i:i+2,j:j+2] 
+            end
+        end
+        Sout[:,:,qi] = output_Sab
+    end
+    return Sout
+end
+
+@inline function orientation_matrix_deprecated(q)
+    q /= norm(q) + 1e-12
+    return (I(3) - q * q')
+end
+
+function intensities_SCGA_deprecated(sys,T,qs;form_factor=nothing,SumRule = "Classical")
+    S = StructureFactorSCGA_deprecated(sys,T,qs;SumRule)
+    int_out = zeros(Float64,length(qs))
+    Na = natoms(sys.crystal)
+    if isnothing(form_factor)
+        for (qi,q) ∈ enumerate(qs) 
+            q_abs = sys.crystal.recipvecs * q
+            int = real(sum(orientation_matrix_deprecated(q_abs).*S[:,:,qi]))/Na
+            int_out[qi] = int
+        end
+    else
+        for (qi,q) ∈ enumerate(qs) 
+            q_abs = sys.crystal.recipvecs * q
+            ff = compute_form_factor(form_factor, norm(q_abs))
+            int = ff*real(sum(orientation_matrix_deprecated(q_abs).*S[:,:,qi]))
+            int_out[qi] = int
+        end
+    end
+    return reshape(int_out,size(qs))
+end
+=#
