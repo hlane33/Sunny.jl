8a51ec212279a4898ce8f89ba830380eda6aca7e
hlane33 <119619760+hlane33@users.noreply.github.com>
2024-09-12 17:32:56 +0100

    Fix form factors

--- src/SCGA/SCGA.jl
+++ src/SCGA/SCGA.jl
@@ -17,8 +17,7 @@ end
 
 function fourier_transform_interaction_matrix(sys::System; k, ϵ=0)
     @assert sys.mode in (:dipole, :dipole_large_S) "SU(N) mode not supported"
-    @assert sys.latsize == (1, 1, 1) "System must have only a single cell"
-
+    @assert sys.dims == (1, 1, 1) "System must have only a single cell"
     Na = natoms(sys.crystal)
     J_k = zeros(ComplexF64, 3, Na, 3, Na)
 
@@ -58,7 +57,7 @@ function fourier_transform_interaction_matrix(sys::System; k, ϵ=0)
 end
 
 
-function find_lagrange_multiplier(sys::System,T; ϵ=0, SumRule = "Classical")
+function find_lagrange_multiplier(sys::System,kT; ϵ=0, SumRule = "Classical")
     Nq = 8
     dq = 1/Nq;
     Na = natoms(sys.crystal)
@@ -73,70 +72,65 @@ function find_lagrange_multiplier(sys::System,T; ϵ=0, SumRule = "Classical")
         error("Unsupported SumRule: $SumRule. Expected 'Classical' or 'Quantum'.")
     end
     println("Sum rule: $SumRule")
-    kb=0.0861733326
     eig_vals = zeros(3Na,length(Jq_array))
     for j in 1:length(Jq_array)
          eig_vals[:,j] .= eigvals(Jq_array[j])
      end
     function loss(λ)
-        sum_term = sum(1 ./ (λ .+ (1/(kb*T)).*eig_vals ))
+        sum_term = sum(1 ./ (λ .+ (1/(kT)).*eig_vals ))
         return ((1/(Na*length(q))) * sum_term - S_sq)^2 
     end
-    lower = maximum(-eig_vals./((1/(kb*T))))
+    lower = maximum(-eig_vals./((1/(kT))))
     upper = Inf
-    p = [lower+20]
+    p = [lower+2]
     result = optimize(loss, lower, upper, p,NelderMead(), Optim.Options(time_limit = 60.0))
     min = Optim.minimizer(result)[1]
     println("Lagrange multiplier: $min")
     return min
 end
 
-function StructureFactorSCGA(sys,T,qs;SumRule = "Classical")
-    Na = natoms(sys.crystal)
-    kb=0.0861733326
-    λ = find_lagrange_multiplier(sys,T;SumRule)
-    Sout = zeros(ComplexF64, 3,3, length(qs))
-    for (qi,q) ∈ enumerate(qs)
-        J_mat = fourier_transform_interaction_matrix(sys; k=q, ϵ=0)
-        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/(kb*T))*J_mat))
-        output_Sab = zeros(ComplexF64, 3,3)
-        index_list = 1:3:3Na 
-        for i ∈ index_list
-            for j ∈ index_list
-                output_Sab += inverted_matrix[i:i+2,j:j+2] 
+function intensities_static(scga::SCGA, qpts; formfactors=nothing, kT=0.0, SumRule = "Classical")
+    kT == 0.0 && error("kT must be non-zero")
+    qpts = convert(AbstractQPoints, qpts)
+    (; sys, measure, regularization) = scga
+    Na = Sunny.natoms(sys.crystal)
+    Nq = length(qpts.qs)
+    Nobs = num_observables(measure)
+    λ = Sunny.find_lagrange_multiplier(sys,kT;SumRule)
+    intensity = zeros(eltype(measure),Nq)
+    Ncorr = length(measure.corr_pairs)
+    for (iq, q) in enumerate(qpts.qs)
+        pref = zeros(ComplexF64, Nobs, Na)
+        corrbuf = zeros(ComplexF64, Ncorr)
+        intensitybuf = zeros(eltype(measure),3,3)
+        q_reshaped = to_reshaped_rlu(sys, q)
+        q_global = sys.crystal.recipvecs * q
+        for i in 1:Na
+            for μ in 1:3
+                ff = get_swt_formfactor(measure, μ, i)
+                pref[μ, i] = compute_form_factor(ff, norm2(q_global))
             end
         end
-        Sout[:,:,qi] = output_Sab
-    end
-    return Sout
-end
-
-@inline function orientation_matrix(q)
-    q /= norm(q) + 1e-12
-    return (I(3) - q * q')
-end
-
-function intensities_SCGA(sys,T,qs;form_factor=nothing,SumRule = "Classical")
-    S = StructureFactorSCGA(sys,T,qs;SumRule)
-    int_out = zeros(Float64,length(qs))
-    Na = natoms(sys.crystal)
-    if isnothing(form_factor)
-        for (qi,q) ∈ enumerate(qs) 
-            q_abs = sys.crystal.recipvecs * q
-            int = real(sum(orientation_matrix(q_abs).*S[:,:,qi]))/Na
-            int_out[qi] = int
+        J_mat = fourier_transform_interaction_matrix(sys; k=q_reshaped, ϵ=regularization)
+        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/kT)*J_mat)) # this is [(Iλ+J(q))^-1]^αβ_μν
+        for i ∈ 1:Na 
+            for j ∈ 1:Na 
+                intensitybuf += pref[1,i]*conj(pref[1,j])*inverted_matrix[1+3(i-1):3+3(i-1),1+3(j-1):3+3(j-1)] 
+                # TODO allow different form factor for each observable 
+            end
         end
-    else
-        for (qi,q) ∈ enumerate(qs) 
-            q_abs = sys.crystal.recipvecs * q
-            ff = compute_form_factor(form_factor, norm(q_abs))
-            int = ff*real(sum(orientation_matrix(q_abs).*S[:,:,qi]))
-            int_out[qi] = int
+        map!(corrbuf, measure.corr_pairs) do (α, β)
+            intensitybuf[α,β]
         end
+        intensity[iq] = measure.combiner(q_global, corrbuf)
     end
-    return reshape(int_out,size(qs))
+    return StaticIntensities(sys.crystal, qpts, reshape(intensity,size(qpts.qs)))
 end
 
+
+###############################################
+###############################################
+#=
 function fourier_transform_deprecated(sys::System; k, ϵ=0)
     @assert sys.mode in (:dipole, :dipole_large_S) "SU(N) mode not supported"
     @assert sys.latsize == (1, 1, 1) "System must have only a single cell"
@@ -170,41 +164,49 @@ function fourier_transform_deprecated(sys::System; k, ϵ=0)
 end
 
 
-function intensities_instant(scga::SCGA, qpts; formfactors=nothing, kT=0.0, SumRule = "Classical")
-    kT == 0.0 && error("kT must be non-zero")
-    qpts = convert(AbstractQPoints, qpts)
-    (; sys, measure, regularization) = scga
+function StructureFactorSCGA_deprecated(sys,T,qs;SumRule = "Classical")
     Na = natoms(sys.crystal)
-    Nq = length(qpts.qs)
-    λ = find_lagrange_multiplier(sys,kT;SumRule)
-    intensity = zeros(eltype(measure),Nq)
-    pref = zeros(ComplexF64, Na)
-    # Temporary storage for pair correlations
-    Ncorr = length(measure.corr_pairs)
-    corrbuf = zeros(ComplexF64, Ncorr)
-    intensitybuf = zeros(eltype(measure),3,3)
-    ff_atoms = propagate_form_factors_to_atoms(formfactors, sys.crystal)
-    for (iq, q) in enumerate(qpts.qs)
-        corrbuf = zeros(ComplexF64, Ncorr)
-        intensitybuf = zeros(eltype(measure),3,3)
-        q_reshaped = to_reshaped_rlu(sys, q)
-        q_global = sys.crystal.recipvecs * q
-        for i in 1:Na
-            pref[i] = Sunny.compute_form_factor(ff_atoms[i], norm2(q_global))
-        end
-        J_mat = fourier_transform_interaction_matrix(sys; k=q_reshaped, ϵ=regularization)
-        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/kT)*J_mat)) # this is [(Iλ+J(q))^-1]^αβ_μν
-        for i ∈ 1:Na 
-            for j ∈ 1:Na 
-                # intensity[:,:,iq] += pref[i]*pref[j]'*inverted_matrix[1+3(i-1):3+3(i-1),1+3(j-1):3+3(j-1)] 
-                intensitybuf += pref[i]*conj(pref[j])*inverted_matrix[1+3(i-1):3+3(i-1),1+3(j-1):3+3(j-1)] 
+    kb=0.0861733326
+    λ = find_lagrange_multiplier(sys,T;SumRule)
+    Sout = zeros(ComplexF64, 3,3, length(qs))
+    for (qi,q) ∈ enumerate(qs)
+        J_mat = fourier_transform_interaction_matrix(sys; k=q, ϵ=0)
+        inverted_matrix = (inv(I(3*Na)*λ[1] + (1/(kb*T))*J_mat))
+        output_Sab = zeros(ComplexF64, 3,3)
+        index_list = 1:3:3Na 
+        for i ∈ index_list
+            for j ∈ index_list
+                output_Sab += inverted_matrix[i:i+2,j:j+2] 
             end
         end
-        map!(corrbuf, measure.corr_pairs) do (α, β)
-            intensitybuf[α,β]
-        end
-        intensity[iq] = measure.combiner(q_global, corrbuf)
+        Sout[:,:,qi] = output_Sab
     end
-    return StaticIntensities(sys.crystal, qpts, intensity)
+    return Sout
 end
 
+@inline function orientation_matrix_deprecated(q)
+    q /= norm(q) + 1e-12
+    return (I(3) - q * q')
+end
+
+function intensities_SCGA_deprecated(sys,T,qs;form_factor=nothing,SumRule = "Classical")
+    S = StructureFactorSCGA_deprecated(sys,T,qs;SumRule)
+    int_out = zeros(Float64,length(qs))
+    Na = natoms(sys.crystal)
+    if isnothing(form_factor)
+        for (qi,q) ∈ enumerate(qs) 
+            q_abs = sys.crystal.recipvecs * q
+            int = real(sum(orientation_matrix_deprecated(q_abs).*S[:,:,qi]))/Na
+            int_out[qi] = int
+        end
+    else
+        for (qi,q) ∈ enumerate(qs) 
+            q_abs = sys.crystal.recipvecs * q
+            ff = compute_form_factor(form_factor, norm(q_abs))
+            int = ff*real(sum(orientation_matrix_deprecated(q_abs).*S[:,:,qi]))
+            int_out[qi] = int
+        end
+    end
+    return reshape(int_out,size(qs))
+end
+=#
