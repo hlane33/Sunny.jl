770131ce4d492062d34c9b831b42f2ec01ff49f8
Hao-Phys <hzhangphys@gmail.com>
2025-01-20 11:33:03 -0700

    Correct typos in the diagonal element of two-particle Hamiltonian

--- src/SpinWaveTheory/TwoParticleHamiltonian.jl
+++ src/SpinWaveTheory/TwoParticleHamiltonian.jl
@@ -1,9 +1,9 @@
-function two_particle_hamiltonian!(H::Matrix{ComplexF64}, npt::NonPerturbativeTheory, qcom_carts_index)
+function two_particle_hamiltonian!(H::Matrix{ComplexF64}, npt::NonPerturbativeTheory, qcom_carts_index::CartesianIndex)
     H .= 0.0
-    (; two_particle_states, swt) = npt
+    (; two_particle_states, swt, clustersize) = npt
+    Nu = clustersize[1] * clustersize[2] * clustersize[3]
     L = nbands(swt)
 
-    qcom_carts_index = CartesianIndex(qcom_carts_index)
     com_states = two_particle_states[qcom_carts_index]
 
     q_pairs = Tuple{Vec3, Vec3, CartesianIndex, CartesianIndex}[]
@@ -53,9 +53,42 @@ function two_particle_hamiltonian!(H::Matrix{ComplexF64}, npt::NonPerturbativeTh
         tuple_i = (state_i.q1, state_i.q2, state_i.q1_carts_index, state_i.q2_carts_index)
         tuple_j = (state_j.q1, state_j.q2, state_j.q1_carts_index, state_j.q2_carts_index)
 
-        # Note that in the definition of the quartic vertex, we've already normalized the result by number of unit cells
+        # The quartic vertex is multiplied by a factor of `Nm` (number of magnetic sites) in 
+        # the magnetic unit cell. This is because the final results are reported in the Brillouin zone of the chemical unit cell.
         pairkey_i = dict[tuple_i]
         pairkey_j = dict[tuple_j]
-        H[com_i, com_j] += δ(i, k) * δ(j, l) * (npt.Es[band1, state_i.q1_carts_index] + npt.Es[band2, state_i.q2_carts_index]) * ζij^2 + ret[band1, band2, band3, band4, pairkey_i, pairkey_j] * ζij * ζkl
+        H[com_i, com_j] += (δ(i, k) * δ(j, l) + δ(i, l) * δ(j, k)) * (npt.Es[band1, state_i.q1_carts_index] + npt.Es[band2, state_i.q2_carts_index]) * ζij^2 + ret[band1, band2, band3, band4, pairkey_i, pairkey_j] * ζij * ζkl / Nu
     end
 end
+
+
+# TODO: integrate this with Lanczos
+# function two_particle_dispersion(npt::NonPerturbativeTheory, qs, niters)
+#     (; clustersize, swt) = npt
+#     Nu1, Nu2, Nu3 = clustersize
+#     # number of two-particle states
+#     L2p = npt.two_particle_states[1, 1, 1]
+#     H2p = zeros(ComplexF64, L2p, L2p)
+#     disp_2p = zeros(Float64, niters, length(qs))
+
+#     qmags = [[i/Nu1, j/Nu2, k/Nu3] for i in 0:Nu1-1, j in 0:Nu2-1, k in 0:Nu3-1]
+
+#     com_indices = CartesianIndex[]
+
+#     # Find the nearest center of mass index in the magnetic Brillouin zone
+#     for q in qs
+#         q_reshaped = to_reshaped_rlu(swt.sys, q)
+#         qcom_carts_index = findmin(x -> norm(x - q_reshaped), qmags)[2]
+#         push!(com_indices, qcom_carts_index)
+#     end
+
+#     for (i, qcom_carts_index) in enumerate(com_indices)
+#         two_particle_hamiltonian!(H2p, npt, qcom_carts_index)
+#         # TODO: Add a check for thehermitianity of the matrix
+#         hermitianpart!(H2p)
+#         H_tridiag = lanczos(H2p, niters)
+#         view(disp_2p, :, i), _ = eigen(H_tridiag) 
+#     end
+
+#     return reshape_dispersions(disp_2p)
+# end
