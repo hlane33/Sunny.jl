d2297fe9eca95e05fca4810992b20a4f473642a6
Harry Lane <ht.lane@outlook.com>
2025-01-28 15:42:12 +0000

    Create functions to print G.

--- src/SCGA/SCGA.jl
+++ src/SCGA/SCGA.jl
@@ -208,7 +208,7 @@ function free_energy_and_gradient(sys,λs,kT;SumRule = "Quantum")
 end
 
 
-function find_lagrange_multiplier_opt_slow(sys,λs,kT;SumRule = "Quantum")
+function find_lagrange_multiplier_opt_slow(sys,λs,kT;SumRule = "Quantum",method = "ConjugateGradient",maxiters=500,tol=1e-10)
     if SumRule == "Classical"
         S_sq = vec(sys.κs.^2)
     elseif SumRule == "Quantum"
@@ -222,50 +222,215 @@ function find_lagrange_multiplier_opt_slow(sys,λs,kT;SumRule = "Quantum")
     qarray = -0.5: dq : 0.5-dq
     q = [[qx, qy, qz] for qx in qarray, qy in qarray, qz in qarray]
     N = length(q)
-    function f(λs)
+    if method == "ConjugateGradient"
+        function f(λs)
+            Λ =  diagm(repeat(λs, inner=3))
+            A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) .+  (1/kT)*Λ for q_in ∈ q] 
+            eig_vals = zeros(3Na,length(A_array))
+            Us = zeros(ComplexF64,3Na,3Na,length(A_array))
+            for j in 1:length(A_array)
+                T = eigen(A_array[j])
+                eig_vals[:,j] .= T.values
+                Us[:,:,j] .= T.vectors
+            end
+            if minimum(eig_vals) < 0 
+                F =  -Inf
+            else
+                F =  0.5*kT*sum(log.(eig_vals))
+            end
+            G = F - 0.5*N*sum(λs.*S_sq)
+            return -G
+        end
+        function g!(storage,λs)
+        # gradient 
+            Λ =  diagm(repeat(λs, inner=3))
+            A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) .+  (1/kT)*Λ for q_in ∈ q] 
+            eig_vals = zeros(3Na,length(A_array))
+            Us = zeros(ComplexF64,3Na,3Na,length(A_array))
+            for j in 1:length(A_array)
+                T = eigen(A_array[j])
+                eig_vals[:,j] .= T.values
+                Us[:,:,j] .= T.vectors
+            end
+            gradF = zeros(ComplexF64,Na)
+            for i ∈ 1:Na
+                gradλ =diagm(zeros(ComplexF64,3Na))
+                gradλ[3i-2:3i,3i-2:3i] =diagm([1,1,1])
+                # gradF[i] =0.5kT*sum([tr(inv(A) * gradλ) for A ∈ A_array])
+                gradF[i] =0.5sum([tr(diagm(1 ./eig_vals[:,j]) * Us[:,:,j]'*gradλ*Us[:,:,j]) for j ∈ 1:length(A_array)]) 
+                # replace this -> we want to use eigenvalues determined above and invariance of trace under change of basis Tr(A (dΛ/dλ) )= Tr(U'A⁻¹UU'(dΛ/dλ)U) = Tr(D⁻¹U'(dΛ/dλ)U)  
+            end
+            gradG = gradF -0.5*N*S_sq
+            storage .= -real(gradG)
+        end
+        upper = Inf
+        lower = -Inf
+        options = Optim.Options(; iterations=5000, show_trace=false,g_tol=1e-8)
+        result = optimize(f, g!,λs , ConjugateGradient(),options)
+        min = Optim.minimizer(result)
+    elseif method == "Newton's Method"
+        function precalculate_matrices(λs)
+            Λ =  diagm(repeat(λs, inner=3))
+            A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) +  (1/kT)*Λ for q_in ∈ q] 
+            eig_vals = zeros(Float64,3Na,length(A_array))
+            Us = zeros(ComplexF64,3Na,3Na,length(A_array))
+            for j in 1:length(A_array)
+                T = eigen(A_array[j])
+                eig_vals[:,j] .= T.values
+                Us[:,:,j] .= T.vectors
+            end
+            return eig_vals, Us, A_array
+        end
+        function G(λs,eig_vals)
+            if minimum(eig_vals) < 0 
+                F =  -10^9
+            else
+                F =  0.5*kT*sum(log.(eig_vals))
+            end
+            Gout = F - 0.5*N*sum(λs.*S_sq)
+            return -real.(Gout)
+        end
+        function Gp(eig_vals, Us) 
+            gradF = zeros(ComplexF64,Na)
+            for i ∈ 1:Na
+                gradλ =diagm(zeros(ComplexF64,3Na))
+                gradλ[3i-2:3i,3i-2:3i] =diagm([1,1,1])
+                gradF[i] =0.5sum([tr(diagm(1 ./eig_vals[:,j]) * Us[:,:,j]'*gradλ*Us[:,:,j]) for j ∈ 1:size(eig_vals,2)]) 
+            end
+            gradG = gradF -0.5*N*S_sq
+            return -real.(gradG)
+        end
+        function Gpp(eig_vals, Us, A_array)
+            Gppmat = zeros(ComplexF64,Na,Na)
+            for i ∈ 1:Na
+                for j ∈ 1:Na
+                    P1 =diagm(zeros(ComplexF64,3Na))
+                    P1[3i-2:3i,3i-2:3i] =diagm([1,1,1])
+                    P2 =diagm(zeros(ComplexF64,3Na))
+                    P2[3j-2:3j,3j-2:3j] =diagm([1,1,1])
+                    # Gppmat[i,j] = -0.5*sum([tr(diagm(1 ./eig_vals[:,q]) * Us[:,:,q]' * P2 * Us[:,:,q] * diagm(1 ./eig_vals[:,q]) *  Us[:,:,q]' * P1 * Us[:,:,q] ) for q ∈ 1:size(eig_vals,2)])
+                    Gppmat[i,j] = -0.5*sum([tr(inv(A_array[qi])  * P2  * inv(A_array[qi]) * P1  ) for qi ∈ 1:size(eig_vals,2)])
+                end
+            end
+            return -real.(Gppmat)
+        end
+        λn = λs
+        lout = []
+        for n ∈ 1:maxiters
+            eig_vals, Us, A_array =  precalculate_matrices(λn) 
+            λ = λn -inv(Gpp(eig_vals, Us,A_array)) * Gp(eig_vals,Us) 
+            if sum(abs.(real.(λ-λn))) < tol 
+                println("Newton's method converged to within tolerance, $tol, after $n steps.")
+                min=λ
+                break
+            else
+            λn = λ
+            end
+        end
+    else
+        throw("Please provide a valid method.")
+    end
+    return real.(min)
+end
+
+function G_fun(sys,λs,kT;)
+    S_sq = vec(sys.κs.^2)
+    Na = Sunny.natoms(sys.crystal)
+    Nq = 8
+    dq = 1/Nq;
+    qarray = -0.5: dq : 0.5-dq
+    q = [[qx, qy, qz] for qx in qarray, qy in qarray, qz in qarray]
+    N = length(q)
+    function precalculate_matrices(λs)
         Λ =  diagm(repeat(λs, inner=3))
-        A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) .+  (1/kT)*Λ for q_in ∈ q] 
-        eig_vals = zeros(3Na,length(A_array))
+        A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) +  (1/kT)*Λ for q_in ∈ q] 
+        eig_vals = zeros(Float64,3Na,length(A_array))
         Us = zeros(ComplexF64,3Na,3Na,length(A_array))
         for j in 1:length(A_array)
             T = eigen(A_array[j])
             eig_vals[:,j] .= T.values
             Us[:,:,j] .= T.vectors
         end
+        return eig_vals, Us, A_array
+    end
+    function G(λs,eig_vals)
         if minimum(eig_vals) < 0 
-            F =  -Inf
+            F =  -10^9
         else
             F =  0.5*kT*sum(log.(eig_vals))
         end
-        G = F - 0.5*N*sum(λs.*S_sq)
-        return -G
+        Gout = F - 0.5*N*sum(λs.*S_sq)
+        return -real.(Gout)
     end
-    function g!(storage,λs)
-    # gradient 
+    eig_vals, Us, A_array =  precalculate_matrices(λs)
+    return G(λs,eig_vals)
+end
+
+function find_lagrange_multiplier_print(sys,λs,kT;maxiters=500)
+    S_sq = vec(sys.κs.^2)
+    Na = Sunny.natoms(sys.crystal)
+    Nq = 8
+    dq = 1/Nq;
+    qarray = -0.5: dq : 0.5-dq
+    q = [[qx, qy, qz] for qx in qarray, qy in qarray, qz in qarray]
+    N = length(q)
+    function precalculate_matrices(λs)
         Λ =  diagm(repeat(λs, inner=3))
-        A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) .+  (1/kT)*Λ for q_in ∈ q] 
-        eig_vals = zeros(3Na,length(A_array))
+        A_array = [(1/kT)*Sunny.fourier_transform_interaction_matrix(sys; k=q_in, ϵ=0) +  (1/kT)*Λ for q_in ∈ q] 
+        eig_vals = zeros(Float64,3Na,length(A_array))
         Us = zeros(ComplexF64,3Na,3Na,length(A_array))
         for j in 1:length(A_array)
             T = eigen(A_array[j])
             eig_vals[:,j] .= T.values
             Us[:,:,j] .= T.vectors
         end
+        return eig_vals, Us, A_array
+    end
+    function G(λs,eig_vals)
+        if minimum(eig_vals) < 0 
+            F =  -10^9
+        else
+            F =  0.5*kT*sum(log.(eig_vals))
+        end
+        Gout = F - 0.5*N*sum(λs.*S_sq)
+        return -real.(Gout)
+    end
+    function Gp(eig_vals, Us) 
         gradF = zeros(ComplexF64,Na)
         for i ∈ 1:Na
             gradλ =diagm(zeros(ComplexF64,3Na))
             gradλ[3i-2:3i,3i-2:3i] =diagm([1,1,1])
-            # gradF[i] =0.5kT*sum([tr(inv(A) * gradλ) for A ∈ A_array])
-            gradF[i] =0.5sum([tr(diagm(1 ./eig_vals[:,j]) * Us[:,:,j]'*gradλ*Us[:,:,j]) for j ∈ 1:length(A_array)]) 
-            # replace this -> we want to use eigenvalues determined above and invariance of trace under change of basis Tr(A (dΛ/dλ) )= Tr(U'A⁻¹UU'(dΛ/dλ)U) = Tr(D⁻¹U'(dΛ/dλ)U)  
+            gradF[i] =0.5sum([tr(diagm(1 ./eig_vals[:,j]) * Us[:,:,j]'*gradλ*Us[:,:,j]) for j ∈ 1:size(eig_vals,2)]) 
         end
         gradG = gradF -0.5*N*S_sq
-        storage .= -real(gradG)
+        return -real.(gradG)
     end
-    upper = Inf
-    lower = -Inf
-    options = Optim.Options(; iterations=5000, show_trace=false,g_tol=1e-8)
-    result = optimize(f, g!,λs , ConjugateGradient(),options)
-    min = Optim.minimizer(result)
-    return min
+    function Gpp(eig_vals, Us, A_array)
+        Gppmat = zeros(ComplexF64,Na,Na)
+        for i ∈ 1:Na
+            for j ∈ 1:Na
+                P1 =diagm(zeros(ComplexF64,3Na))
+                P1[3i-2:3i,3i-2:3i] =diagm([1,1,1])
+                P2 =diagm(zeros(ComplexF64,3Na))
+                P2[3j-2:3j,3j-2:3j] =diagm([1,1,1])
+                # Gppmat[i,j] = -0.5*sum([tr(diagm(1 ./eig_vals[:,q]) * Us[:,:,q]' * P2 * Us[:,:,q] * diagm(1 ./eig_vals[:,q]) *  Us[:,:,q]' * P1 * Us[:,:,q] ) for q ∈ 1:size(eig_vals,2)])
+                Gppmat[i,j] = -0.5*sum([tr(inv(A_array[qi])  * P2  * inv(A_array[qi]) * P1  ) for qi ∈ 1:size(eig_vals,2)])
+            end
+        end
+        return -real.(Gppmat)
+    end
+    λn = λs
+    lout = [λn]
+    for n ∈ 1:maxiters
+        eig_vals, Us, A_array =  precalculate_matrices(λn) 
+        try
+            λ = λn -inv(Gpp(eig_vals, Us,A_array)) * Gp(eig_vals,Us) 
+            λn = λ
+        catch e 
+            println("unstable!")
+            break
+        end
+        push!(lout,λn)
+    end
+    return lout
 end
