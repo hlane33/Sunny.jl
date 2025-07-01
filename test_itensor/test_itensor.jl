87d365b7e946232f45626a02595a27582b599111
Adam <adam@admins-iMac.local>
2025-07-01 09:18:29 +0100

    Added Itensor MPS and worked through examples that set up spin systems

--- test_itensor/test_itensor.jl
+++ test_itensor/test_itensor.jl
@@ -1,7 +1,23 @@
-using ITensors
+using ITensors, ITensorMPS
+let
+  N = 100
+  sites = siteinds("S=1",N)
 
-# Example: create a simple ITensor and print it
-i = Index(2, "i")
-T = randomITensor(i)
-println("Random ITensor:")
-println(T)
+  os = OpSum()
+  for j=1:N-1
+    os += "Sz",j,"Sz",j+1
+    os += 1/2,"S+",j,"S-",j+1
+    os += 1/2,"S-",j,"S+",j+1
+  end
+  H = MPO(os,sites)
+
+  psi0 = random_mps(sites;linkdims=10)
+
+  nsweeps = 5
+  maxdim = [10,20,100,100,200]
+  cutoff = [1E-10]
+
+  energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
+
+  return
+end
