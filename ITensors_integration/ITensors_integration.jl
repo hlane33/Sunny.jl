a9a9501d4a2fe7efe92aac76046e7353f58c3a13
Adam <adam@Adams-iMac.local>
2025-08-12 17:44:15 +0100

    Create module like file to combine all  code together

---
+++ ITensors_integration/ITensors_integration.jl
@@ -0,0 +1,11 @@
+#====#
+#This holds all of the ITensors integration code, which is used to integrate
+#Sunny with the ITensors.jl package for tensor network simulations.
+
+#It could be turned into a module/ absorbed into Sunny if desired.
+#====#
+include("sunny_toITensor.jl")
+include("MeasuredCorrelations.jl")
+include("overloaded_intensities.jl")
+include("useful_TDVP_functions.jl")
+include("CorrelationMeasuring.jl")
