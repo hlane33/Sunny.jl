f9f21b4e73ec912ecc6aa5b865eb72c0d73e8b59
Harry Lane <ht.lane@outlook.com>
2026-08-11 17:09:15 +0100

    finish self consistency and optimize mean fields

--- src/RPA/RPA_aliasing.jl
+++ src/RPA/RPA_aliasing.jl
@@ -244,3 +244,142 @@ function sites_to_internal_bond(sys::ElectronicSystem, site1::CartesianIndex{4},
     end
 end
 
+struct RPA 
+    sys            :: ElectronicSystem
+    # data           :: Union{SWTDataDipole, SWTDataSUN}
+    measure        :: MeasureSpec
+    regularization :: Float64
+end
+
+function RPA(sys::ElectronicSystem; measure::Union{Nothing, MeasureSpec}, regularization=1e-8, energy_ϵ=nothing)
+    if !isnothing(energy_ϵ)
+        @warn "Keyword argument energy_ϵ is deprecated! Use `regularization` instead."
+        regularization = energy_ϵ
+    end
+
+    measure = @something measure empty_measurespec(sys)
+    if size(eachsite(sys)) != size(measure.observables)[2:5]
+        error("Size mismatch. Check that measure is built using consistent system.")
+    end
+
+    # Create a single enlarged chemical cell that matches the full system size
+    # while preserving the system's linear site order.
+    new_cryst = resize_and_flatten_crystal(sys.crystal, sys.dims)
+
+    # Create a new system with dims (1,1,1). A clone happens in all cases.
+    sys = reshape_supercell_aux(sys, new_cryst, (1,1,1))
+
+    # Rotate local operators to quantization axis
+    # data = swt_data(sys, measure)
+    # TODO figure out data for this struct
+    # return SpinWaveTheory(sys, data, measure, regularization)
+    return RPA(sys, measure, regularization)
+end
+
+function all_dipole_observables(sys::ElectronicSystem; )
+    observables = zeros(Vec3, 3, size(eachsite(sys))...)
+    for site in eachsite(sys)
+        # Component α of observable is op⋅S = g[α,β] S[β]. Minus sign would
+        # cancel because observables come in pairs.
+        op =  Mat3(I)
+        for α in 1:3
+            observables[α, site] = op[α, :]
+        end
+    end
+    return observables
+end
+
+function ssf_custom(f, sys::ElectronicSystem;  formfactors=nothing)
+    observables = all_dipole_observables(sys; )
+    corr_pairs = [(3,3), (2,3), (1,3), (2,2), (1,2), (1,1)]
+    combiner(q, corr) = f(q, SA[
+        corr[6]       corr[5]       corr[3]
+        conj(corr[5]) corr[4]       corr[2]
+        conj(corr[3]) conj(corr[2]) corr[1]
+    ])
+    formfactors = if isnothing(formfactors)
+        fill(one(FormFactor), natoms(sys.crystal))
+    else
+        formfactors isa Vector{Pair{Int, FormFactor}} || error("Pass formfactors as [i1 => FormFactor(...), i2 => ...]")
+        propagate_atom_data(orig_crystal(sys), sys.crystal, formfactors)
+    end
+    return MeasureSpec(observables, corr_pairs, combiner, formfactors)
+end
+
+
+function ssf_custom_bm(f, sys::ElectronicSystem; u, v, formfactors=nothing)
+    u = orig_crystal(sys).recipvecs * u
+    v = orig_crystal(sys).recipvecs * v
+    e3 = normalize(u × v)
+
+    return ssf_custom(sys::ElectronicSystem;  formfactors) do q, ssf
+        if iszero(q)
+            error("Blume-Maleev axis system not defined at zero q")
+        end
+        if abs(q ⋅ e3) > 1e-12
+            error("Momentum transfer q not in scattering plane")
+        end
+        e1 = normalize(q)      # parallel to q
+        e2 = normalize(e3 × q) # perpendicular to q, in the scattering plane
+        bm = hcat(e1, e2, e3)  # Blume-Maleev axis system
+        f(Vec3(norm(q), 0, 0), bm' * ssf * bm)
+    end
+end
+
+"""
+    ssf_perp(sys::System; apply_g=true, formfactors=nothing)
+
+Specify measurement of the spin structure factor with contraction by
+``(I-𝐪⊗𝐪/q^2)``. The contracted value provides an estimate of unpolarized
+scattering intensity. In the singular limit ``𝐪 → 0``, the contraction matrix
+is replaced by its rotational average, ``(2/3) I``.
+
+This function is a special case of [`ssf_custom`](@ref).
+
+# Example
+
+```julia
+# Select Co²⁺ form factor for atom 1 and its symmetry equivalents
+formfactors = [1 => FormFactor("Co2")]
+ssf_perp(sys; formfactors)
+```
+"""
+function ssf_perp(sys::ElectronicSystem;  formfactors=nothing)
+    return ssf_custom(sys;  formfactors) do q, ssf
+        q2 = norm2(q)
+        # Imaginary part vanishes in symmetric contraction
+        ssf = real(ssf)
+        # "S-perp" contraction matrix (1 - q⊗q/q²) appropriate to unpolarized
+        # neutrons. In the limit q → 0, use (1 - q⊗q/q²) → 2/3, which
+        # corresponds to a spherical average over uncorrelated data:
+        # https://github.com/SunnySuite/Sunny.jl/pull/131
+        (iszero(q2) ? (2/3)*tr(ssf) : tr(ssf) - dot(q, ssf, q) / q2)
+    end
+end
+
+"""
+    ssf_trace(sys::System; apply_g=true, formfactors=nothing)
+
+Specify measurement of the spin structure factor, with trace over spin
+components. This quantity can be useful for checking quantum sum rules.
+
+This function is a special case of [`ssf_custom`](@ref).
+"""
+function ssf_trace(sys::ElectronicSystem; formfactors=nothing)
+    return ssf_custom(sys; formfactors) do q, ssf
+        tr(real(ssf))
+    end
+end
+
+
+# Maps atom `i` in `cryst` to the corresponding site in `other_sys`
+function map_atom_to_other_system(cryst::Crystal, i, other_sys::ElectronicSystem)
+    global_r = cryst.latvecs * cryst.positions[i]
+    other_r = orig_crystal(other_sys).latvecs \ global_r
+    return position_to_site(other_sys, other_r)
+end
+
+function to_standard_rlu(sys::ElectronicSystem, q_reshaped)
+    return orig_crystal(sys).recipvecs \ (sys.crystal.recipvecs * q_reshaped)
+end
+
